87e2ca7c115f3b299309e61396434042cdff5ab7
ZJW <zhoujiawei@joygames.cc>
2019-01-10 10:51:23 +0800

    新增GameRoot.cs

---
+++ Assets/Scripts/GameRoot.cs
@@ -0,0 +1,15 @@
+/****************************************************
+    文件：GameRoot.cs
+	作者：Jerry
+    邮箱: riverlihuo@163.com
+    日期：2019/1/10 10:50:48
+	功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+using UnityEditor;
+
+public class GameRoot : MonoBehaviour 
+{
+
+}
